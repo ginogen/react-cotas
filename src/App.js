dbbab4b9a070f4d0e2315ac1f3d02f8d97adf64c
ginogen <ginogentile@live.com>
2019-01-28 18:36:58 -0300

    probando

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1 className="App-Title">
+        POKEMASCOTAS
+        </h1>
+        <header className="App-header">
+          
+          
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
